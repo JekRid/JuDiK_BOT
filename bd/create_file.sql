96f31ad9279b199c95334c3427d88d0fea99a8ff
sarangdi <gorrazh@gmail.com>
2022-10-29 18:16:55 +0500

    changed

--- bd/create_file.sql
+++ bd/create_file.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE worker(
     id_worker integer NOT NULL,
 	id_tg VARCHAR(50),
@@ -11,15 +9,13 @@ CREATE TABLE worker(
 	PRIMARY KEY("id_worker"));
 	
 
-
 CREATE TABLE event (
  id_event integer NOT NULL,
  event_name VARCHAR(120),
  date_event char(30),
  time_event char(30), 
  place_event VARCHAR(40),
- PRIMARY KEY("id_event"));
-
+ PRIMARY KEY("id_event")); 
 
 
 create table schedule (
@@ -27,7 +23,5 @@ id_event integer NOT NULL,
 id_worker integer NOT NULL,
 FOREIGN KEY("id_event") REFERENCES event("id_event"),
 FOREIGN KEY("id_worker") REFERENCES worker("id_worker"));
-
-
 
 

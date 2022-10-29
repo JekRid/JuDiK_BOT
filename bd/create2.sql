1a6318c368a5bff64dab9d9499c8fbb5c46f74d5
sarangdi <gorrazh@gmail.com>
2022-10-29 19:57:22 +0500

    SDELANO

---
+++ bd/create2.sql
@@ -0,0 +1,36 @@
+	
+CREATE TABLE department(
+    id_dep integer NOT NULL,
+	dep_name VARCHAR(50),
+	PRIMARY KEY("id_dep"));
+	
+
+CREATE TABLE worker(
+    id_worker integer NOT NULL,
+	id_tg VARCHAR(50),
+    fam_worker VARCHAR(50),
+	name_worker VARCHAR(50),
+	otch_worker VARCHAR(50),
+	id_dep integer NOT NULL,
+	login VARCHAR(50),
+	password VARCHAR(50),
+	PRIMARY KEY("id_worker"),
+    FOREIGN KEY("id_dep") REFERENCES department("id_dep"));
+	
+
+CREATE TABLE event (
+ id_event integer NOT NULL,
+ event_name VARCHAR(120),
+ date_event date,
+ time_event time, 
+ place_event VARCHAR(40),
+ PRIMARY KEY("id_event")); 
+
+
+create table schedule (
+id_event integer NOT NULL,
+id_worker integer NOT NULL,
+FOREIGN KEY("id_event") REFERENCES event("id_event"),
+FOREIGN KEY("id_worker") REFERENCES worker("id_worker"));
+
+
